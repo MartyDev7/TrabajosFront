eaaf0e803a8d586e34972132d23deb0055dc25c1
MartyDev7 <155499928+MartyDev7@users.noreply.github.com>
2024-12-05 12:29:55 -0600

    Comentarios

--- Basic/00-helloworld.js
+++ Basic/00-helloworld.js
@@ -1,1 +1,10 @@
-console.log("Hola mundo")
+//esto es un comentario simple
+
+/*
+esto es un comentario
+en varias lineas
+con JS
+*/
+
+console.log("Hola mundo") //texto con dobles comillas
+console.log('Hola mundo') //texto con comillas simples
