7c623b18fc7c386ab927e7a31b06482805f657d0
MartyDev7 <155499928+MartyDev7@users.noreply.github.com>
2024-12-05 22:45:22 -0600

    los siete tipos de datos

---
+++ Basic/02-dataTypes.js
@@ -0,0 +1,43 @@
+//tipos de datos
+//primitivos que son básicos(inmutables que representan un solo valor)
+
+//cadena de texto (String)
+let myName = "Juan Carlos"
+let alias = 'Marty'
+let email = "juanjs@gmail.com"
+
+//numericos
+//int (enteros)
+let ege = 22
+
+//float
+let altura = 1.25
+
+//booleanos (boolean)
+let isStudent = true
+let isTeacher = false
+
+//Undefined (no definido)
+let undefinedValue
+console.log(undefinedValue)
+
+//null
+let nullValue = null
+
+//Symbol
+let mySymbol = Symbol("mysymbol")
+
+//BygInt
+let myBigInt = BigInt(151543484845845184548454541545457461541654521)
+let myBigInt2 = 151543484845845184548454541545457461541654521n
+
+
+//mostrar el tipo de datos
+console.log(typeof myName)
+console.log(typeof altura)
+console.log(typeof isStudent)
+console.log(typeof undefinedValue)
+console.log(typeof nullValue)
+console.log(typeof mySymbol)
+console.log(typeof myBigInt)
+
