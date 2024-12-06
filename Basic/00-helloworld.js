977d1d310395d69998a5d377fc9db8f076871eee
MartyDev7 <155499928+MartyDev7@users.noreply.github.com>
2024-12-05 22:44:06 -0600

    Tipos de datos

--- Basic/00-helloworld.js
+++ Basic/00-helloworld.js
@@ -8,3 +8,9 @@ con JS
 
 console.log("Hola mundo") //texto con dobles comillas
 console.log('Hola mundo') //texto con comillas simples
+
+//tipos de datos
+console.log(5)
+console.log("5")
+console.log(5 + 2)
+console.log(5 - 2)
