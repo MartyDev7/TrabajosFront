1e669531603ed0e6632137dcca2df5f7fa27426b
MartyDev7 <155499928+MartyDev7@users.noreply.github.com>
2024-12-05 22:44:45 -0600

    Tipos de variables y como utilizarlas

---
+++ Basic/01-variables.js
@@ -0,0 +1,22 @@
+//tipos de variables
+//var
+var helloWorld = "Hola JavaScript"
+console.log(helloWorld)
+
+helloWorld = "Hola de nuevo"
+console.log(helloWorld)
+
+//let
+let helloWorld2 = "Hola Mundo dos"
+console.log(helloWorld2)
+
+helloWorld2 = "Hola de nuevo dos"
+console.log(helloWorld2)
+
+
+//const
+const helloWorld3 = "Hola mundo tres"
+console.log(helloWorld3)
+
+helloWorld3 = "Hola de nuevo mundo tres"
+console.log(helloWorld3)
