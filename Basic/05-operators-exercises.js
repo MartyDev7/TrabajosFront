3f777c95e3ae04ea53b8d61254cedce935b4e413
MartyDev7 <155499928+MartyDev7@users.noreply.github.com>
2024-12-14 20:19:22 -0600

    First excercise

---
+++ Basic/05-operators-exercises.js
@@ -0,0 +1,48 @@
+// 1. Crea una variable para cada operación aritmética
+//aritmeticos
+let a = 10
+let b = 62
+
+let suma
+suma = a + b
+console.log("La suma es: " + suma)
+
+let resta
+resta = a - b
+console.log("La resta es: " + resta)
+
+let multiplicacion
+multiplicacion = a * b
+console.log("La multiplicacion es: " + multiplicacion)
+
+let division
+division = a / b
+console.log("la division es: " + division)
+
+let modulo
+modulo = a % b
+console.log("El módulo restante es: " + modulo)
+
+let exponente
+exponente = a**b
+console.log("El exponente de: " + a + " a la " + b +" es: " + exponente)
+
+
+// 2. Crea una variable para cada tipo de operación de asignación,
+//    que haga uso de las variables utilizadas para las operaciones aritméticas
+
+// 3. Imprime 5 comparaciones verdaderas con diferentes operadores de comparación
+
+// 4. Imprime 5 comparaciones falsas con diferentes operadores de comparación
+
+// 5. Utiliza el operador lógico and
+
+// 6. Utiliza el operador lógico or
+
+// 7. Combina ambos operadores lógicos
+
+// 8. Añade alguna negación
+
+// 9. Utiliza el operador ternario
+
+// 10. Combina operadores aritméticos, de comparáción y lógicas
